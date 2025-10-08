5a69492d3a3a4d77adbd33eb185ab8028b1d559d
Jhon-Quiceno <quicenojale@gmail.com>
2025-10-07 19:57:38 -0500

    feat: admin autentication con jwt

--- src/main/java/com/backend/estudiantes/controller/AuthController.java
+++ src/main/java/com/backend/estudiantes/controller/AuthController.java
@@ -3,14 +3,18 @@ package com.backend.estudiantes.controller;
 import com.backend.estudiantes.dto.LoginRequest;
 import com.backend.estudiantes.model.Usuario;
 import com.backend.estudiantes.service.AuthService;
+import com.backend.estudiantes.service.JwtService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -20,19 +24,34 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+    @Autowired
+    private JwtService jwtService;
+
     //Endpoint para login
     @PostMapping("/login")
     public ResponseEntity<?> login(@Valid @RequestBody LoginRequest request) {
         try {
             Usuario usuario = authService.authenticate(request.email(), request.password());
+
+            Map<String, Object> extraClaims = new HashMap<>();
+            extraClaims.put("rol", usuario.getRol().name());
+            extraClaims.put("nombre", usuario.getNombre());
+            extraClaims.put("email", usuario.getEmail());
+
+            String jwtToken = jwtService.generateToken(extraClaims, usuario);
+
             return ResponseEntity.ok(Map.of(
                     "message", "Login exitoso!",
-                    "email", usuario.getEmail(),
-                    "rol", usuario.getRol().name()
+                    "token", jwtToken,
+                    "expiresIn", jwtService.getJwtExpirationMs(),
+                    "data", Map.of(
+                            "email", usuario.getEmail(),
+                            "rol", usuario.getRol().name()
+                    )
             ));
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body(Map.of(
-                    "error", e.getMessage()
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of(
+                    "Error", "Credenciales incorrectas"
             ));
         }
     }
--- src/main/java/com/backend/estudiantes/model/Usuario.java
+++ src/main/java/com/backend/estudiantes/model/Usuario.java
@@ -2,6 +2,11 @@ package com.backend.estudiantes.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "usuarios")
@@ -9,7 +14,7 @@ import lombok.*;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -40,5 +45,35 @@ public class Usuario {
         this.email = email;
         this.password = password;
         this.rol = rol;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
     }
 }
